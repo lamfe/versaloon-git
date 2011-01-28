1a91a6c79bb8e6f372e8ff1cb5b487e28128bf12
Simon Qian <simonqian@simonqian.com>
2011-01-28 20:04:40 +0000

    reset after programming stm32 flash

--- src/target/stm32/stm32.c
+++ src/target/stm32/stm32.c
@@ -46,7 +46,7 @@
 
 struct program_area_map_t stm32_program_area_map[] = 
 {
-	{APPLICATION_CHAR, 1, 0, 0, 0, AREA_ATTR_EWR | AREA_ATTR_RAE},
+	{APPLICATION_CHAR, 1, 0, 0, 0, AREA_ATTR_EWR | AREA_ATTR_RAE | AREA_ATTR_RAW},
 	{FUSE_CHAR, 0, 0, 0, 0, AREA_ATTR_WR},
 	{0, 0, 0, 0, 0, 0}
 };
