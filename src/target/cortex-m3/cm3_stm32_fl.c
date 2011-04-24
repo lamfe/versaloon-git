a65b00cd02c5e651f29e16198b5aed647ef2fb77
Simon Qian <simonqian@simonqian.com>
2011-04-24 17:26:12 +0000

    use pc offset instead of if-else structure in stm32 flash loader

--- src/target/cortex-m3/cm3_stm32_fl.c
+++ src/target/cortex-m3/cm3_stm32_fl.c
@@ -40,6 +40,12 @@
 #define STM32_FL_COMMAND_OFFSET				0x80
 #define STM32_FL_SYNC_OFFSET				0xA4
 
+#define STM32_FL_DATATYPE_0					(uint32_t)40
+#define STM32_FL_DATATYPE_BYTE				(uint32_t)-2
+#define STM32_FL_DATATYPE_HWORD				(uint32_t)8
+#define STM32_FL_DATATYPE_WORD				(uint32_t)18
+#define STM32_FL_DATATYPE_DWORD				(uint32_t)28
+
 static uint32_t stm32swj_fl_cnt = 0;
 static uint32_t stm32swj_fl_base = 0;
 static uint8_t fl_code[] = {
@@ -65,53 +71,52 @@ static uint8_t fl_code[] = {
 	0x20, 0x4D,					/* ldr.n	r5, [PC, #0x80] ;load DATA_SIZE */
 								/* clear_sync: */
 	0x00, 0x26,					/* movs		r6, #0 */
-	0x20, 0xA7,					/* adr.n	r7, PC, #0x84 */
+	0x20, 0xA7,					/* adr.n	r7, PC, #0x80 */
 	0x3E, 0x60,					/* str		r6, [r7] */
 								/* check_data_size */
 	0x2D, 0x42,					/* tst		r5, r5 */
 	0xEA, 0xD0,					/* beq.n	wait_start */
-								/* check_data_type */
-	0xA6, 0x1B,					/* subs		r6, r4, r6 */
-	0x00, 0xD1,					/* bne.n	write_data */
-	0x0F, 0xE0,					/* b		wait_busy */
 								/* write_data: */
-	0x01, 0x26,					/* movs		r1, #1 */
-	0xA6, 0x1B,					/* subs		r6, r4, r6 */
-	0x02, 0xD1,					/* be.n		write_word_dword */
+	0xA7, 0x44,					/* add		pc, pc, r4 */
+								/* write_byte:				;offset:-2 */
 	0x1F, 0x78,					/* ldrb		r7, [r3] */
 	0x17, 0x70,					/* strb		r7, [r2] */
-	0x07, 0xE0,					/* b.n		adjust_addr */
-								/* write_word_dword: */
-	0x02, 0x26,					/* movs		r6, #2 */
-	0xA6, 0x1B,					/* subs		r6, r4, r6 */
-	0x02, 0xD1,					/* be.n		write_dword */
+	0x01, 0x32,					/* adds		r2, #1 */
+	0x01, 0x33,					/* adds		r3, #1 */
+	0x0F, 0xE0,					/* b.n		wait_busy */
+								/* write_hword:				;offset:8 */
 	0x1F, 0x88,					/* ldrh		r7, [r3] */
 	0x17, 0x80,					/* str		r7, [r2] */
-	0x01, 0xE0,					/* b.n		adjust_addr */
-								/* wrire_dword: */
+	0x02, 0x32,					/* adds		r2, #1 */
+	0x02, 0x33,					/* adds		r3, #1 */
+	0x0A, 0xE0,					/* b.n		wait_busy */
+								/* write_word:				;offset:18 */
+	0x1F, 0x68,					/* ldrh		r7, [r3] */
+	0x17, 0x60,					/* str		r7, [r2] */
+	0x04, 0x32,					/* adds		r2, #1 */
+	0x04, 0x33,					/* adds		r3, #1 */
+	0x05, 0xE0,					/* b.n		wait_busy */
+								/* write_dword:				;offset:28 */
 	0x1F, 0x68,					/* ldr		r7, [r3] */
 	0x17, 0x60,					/* str		r7, [r2] */
-								/* adjust_addr: */
-	0x22, 0x44,					/* add		r2, r2, r4 */
-	0x23, 0x44,					/* add		r3, r3, r4 */
-								/* wait_busy: */
+	0x5F, 0x68,					/* ldr		r7, [r3, #4] */
+	0x57, 0x60,					/* str		r7, [r2, #4] */
+	0x08, 0x32,					/* adds		r2, #1 */
+	0x08, 0x33,					/* adds		r3, #1 */
+								/* wait_busy:				;offset:40 */
 	0x07, 0x68,					/* ldr		r7, [r0] */
 	0x0F, 0x40,					/* ands		r7, r7, r1 */
 	0xFC, 0xD1,					/* bne.n	wait_busy */
 								/* check_all_written: */
 	0x6D, 0x1E,					/* subs		r5, r5, #1 */
-	0xEA, 0xD1,					/* bne.n	write_data */
+	0xE4, 0xD1,					/* bne.n	write_data */
 								/* increase_result */
-	0x13, 0x48,					/* ldr.n	r0, [PC, #0x4C] */
+	0x11, 0x48,					/* ldr.n	r0, [PC, #0x44] */
 	0x40, 0x1C,					/* adds		r0, r0, #1 */
-	0x12, 0xA1,					/* adr.n	r1, PC, #0x48 */
+	0x10, 0xA1,					/* adr.n	r1, PC, #0x40 */
 	0x08, 0x60,					/* str		r0, [r1] */
-	0xCD, 0xE7,					/* b.n		wait_start */
+	0xCA, 0xE7,					/* b.n		wait_start */
 								/* exit: */
-								/* write_result_false: */
-	0x00, 0x20,					/* movs		r0, #0 */
-	0x20, 0xA1,					/* adr.n	r1, PC, #0x80 */
-	0x08, 0x60,					/* str		r0, [r1] */
 								/* dead_loop: */
 	0xFE, 0xE7,					/* b $ */
 	
@@ -120,7 +125,7 @@ static uint8_t fl_code[] = {
 	0x00, 0x00, 0x00, 0x00,
 	0x00, 0x00, 0x00, 0x00,
 	0x00, 0x00, 0x00, 0x00,
-	
+								/* offset:0x100 */
 	0x00, 0x00, 0x00, 0x00,		/* cr_addr */
 	0x00, 0x00, 0x00, 0x00,		/* cr_value1 */
 	0x00, 0x00, 0x00, 0x00,		/* cr_value2 */
@@ -196,7 +201,26 @@ RESULT stm32swj_fl_run(struct stm32_fl_cmd_t *cmd)
 	buff_tmp[4] = SYS_TO_LE_U32(cmd->sr_busy_mask);
 	buff_tmp[5] = SYS_TO_LE_U32(cmd->target_addr);
 	buff_tmp[6] = SYS_TO_LE_U32(cmd->ram_addr);
-	buff_tmp[7] = SYS_TO_LE_U32(cmd->data_type);
+	switch (cmd->data_type)
+	{
+	case 0:
+		buff_tmp[7] = STM32_FL_DATATYPE_0;
+		break;
+	case 1:
+		buff_tmp[7] = STM32_FL_DATATYPE_BYTE;
+		break;
+	case 2:
+		buff_tmp[7] = STM32_FL_DATATYPE_HWORD;
+		break;
+	case 4:
+		buff_tmp[7] = STM32_FL_DATATYPE_WORD;
+		break;
+	case 8:
+		buff_tmp[7] = STM32_FL_DATATYPE_DWORD;
+		break;
+	default:
+		return ERROR_FAIL;
+	}
 	buff_tmp[8] = SYS_TO_LE_U32(cmd->data_size);
 	buff_tmp[9] = SYS_TO_LE_U32(1);
 	
