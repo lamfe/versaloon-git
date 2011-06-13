1b495bf0cf16662d9f693c905463b2e933417a37
Simon Qian <simonqian@simonqian.com>
2011-06-13 07:50:04 +0000

    rename usb_protocol.c to usb_protocol_Versaloon.c

--- dongle/firmware/usb_protocol.h
+++ dongle/firmware/usb_protocol.h
@@ -9,10 +9,6 @@
 #include "stack/usb_device/class/HID/vsfusbd_HID.h"
 #include "stack/usb_device/class/MSC/vsfusbd_MSC_BOT.h"
 
-extern volatile uint32_t cmd_len;
-extern uint8_t buffer_out[USB_DATA_BUFF_SIZE];
-extern volatile uint32_t count_out, usb_ovf;
-
 extern struct vsfusbd_device_t usb_device;
 RESULT usb_protocol_init();
 
