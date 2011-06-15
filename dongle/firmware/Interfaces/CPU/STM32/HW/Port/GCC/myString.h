ac304852d68c23911f2eef0cf397a712902fae21
Simon Qian <simonqian@simonqian.com>
2011-06-15 11:17:48 +0000

    remove unused files in firmware

--- dongle/firmware/Interfaces/CPU/STM32/HW/Port/GCC/myString.h
+++
@@ -1,7 +0,0 @@
-#ifdef USE_BUILDIN_STRING_H
-#include <string.h>
-#else
-extern void* memcpy(void *m0, const void *m1, uint32_t len);
-extern int memcmp(const void *m0, const void *m1, uint32_t len);
-extern void *memset(void *m, int c, uint32_t len);
-#endif
