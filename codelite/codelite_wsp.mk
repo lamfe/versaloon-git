aa13b3ecdae25f186225758db4fde41fe3d9476f
Simon Qian <simonqian@simonqian.com>
2012-01-11 08:20:12 +0000

    remove unnecessary mk files in codelite project

--- codelite/codelite_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ vsprog - Debug ]----------
-	@cd "vsprog" && "mingw32-make.exe"  -j 2 -f "vsprog.mk"
-clean:
-	@echo ----------Cleaning project:[ vsprog - Debug ]----------
-	@cd "vsprog" && "mingw32-make.exe"  -j 2 -f "vsprog.mk" clean
