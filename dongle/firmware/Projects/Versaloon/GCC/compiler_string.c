0906407bc90dbfe01413add6a4bd623a96080112
Simon Qian <simonqian@simonqian.com>
2012-01-20 07:38:59 +0000

    add syscall.c for embedded vsprog, update linker scripts

--- dongle/firmware/Projects/Versaloon/GCC/compiler_string.c
+++ dongle/firmware/Projects/Versaloon/GCC/compiler_string.c
@@ -1,35 +1,127 @@
 #include "app_cfg.h"
 
+#include <stdlib.h>
+
 #ifndef USE_BUILDIN_STRING_H
-void* memcpy(void *m0, const void *m1, int len)
+
+
+int strlen(const char *str)
 {
-	char *ret = m0;
+	const char *p = str;
+	
+	while (*p++);
+	return (int)(p - str - 1);
+}
 
+int strcmp(const char *str0, const char *str1)
+{
+	for (; *str0 && *str1 && *str0 == *str1; str0++, str1++);
+	return (*str0 - *str1);
+}
+
+char* strcpy(char *dest, const char *source)
+{
+	char *p = dest;
+	
+	if (*source)
+	{
+		while (*p++ = *source++);
+		*p = '\0';
+	}
+	return dest;
+}
+
+char* strncpy(char *dest, const char *source, int len)
+{
+	char *p = dest;
+	
+	if (*source)
+	{
+		while (len-- && (*p++ = *source++));
+		*p = '\0';
+	}
+	return dest;
+}
+
+char* strchr(const char *str, int c)
+{
+	while (*str && *str != (char)c) str++;
+	return (*str == (char)c) ? (char *)str : (char *)NULL;
+}
+
+char* strstr(const char *str, const char *substr)
+{
+	char *pstr, *psub;
+	
+	if (!*substr)
+		return (char *)NULL;
+	while (*str)
+	{
+		pstr = (char *)str;
+		psub = (char *)substr;
+		do {
+			if (!*psub) return pstr;
+		} while (*pstr++ == *psub++);
+		str++;
+	}
+	return (char *)NULL;
+}
+
+char* strcat(char *dest, const char *source)
+{
+	char *p = dest;
+	
+	while (*p++);
+	if (*source)
+	{
+		while (*p++ = *source++);
+		*p = '\0';
+	}
+	return dest;
+}
+
+char* strncat(char *dest, const char *source, int len)
+{
+	char *p = dest;
+	
+	while (*p++);
+	while (len-- && (*p++ = *source++));
+	*p = '\0';
+	return dest;
+}
+
+char* strdup(const char *source)
+{
+	char *p = (char *)malloc(strlen(source) + 1);
+	return strcpy(p, source);
+}
+
+void* memcpy(void *dest, const void *source, int len)
+{
+	char *p = dest;
+	
 	while(len--)
-		*(char*)m0++ = *(char*)m1++;
-
-	return ret;
+		*(char*)p++ = *(char*)source++;
+	return dest;
 }
 
 int memcmp(const void *m0, const void *m1, int len)
 {
 	const char *u8_0 = (const char *)m0;
 	const char *u8_1 = (const char *)m1;
-
+	
 	for (; len > 0; ++u8_0, ++u8_1, --len)
 		if (*u8_0 != *u8_1)
 			return (*u8_0 - *u8_1);
-
 	return 0;
 }
 
 void *memset(void *m, int c, int len)
 {
-	const char u8_c = (char)c;
-	char *ptr = (char *)m;
-
+	char *p = (char *)m;
+	
 	for (; len > 0; len--)
-		*ptr++ = u8_c;
+		*p++ = (char)c;
 	return m;
 }
 
