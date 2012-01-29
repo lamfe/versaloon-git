573167c1c50568515532387305c9a67a6003def2
Simon Qian <simonqian@simonqian.com>
2012-01-29 16:21:32 +0000

    add SDIO and SPI_SDIO modules in GCC makefile

--- dongle/firmware/Projects/Versaloon/GCC/syscall.c
+++ dongle/firmware/Projects/Versaloon/GCC/syscall.c
@@ -1,19 +1,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-extern int  __HEAP_START;
-
-static unsigned char *heap = NULL;
+static char *heap = NULL;
 caddr_t _sbrk(int incr)
 {
-	unsigned char *prev_heap;
+	extern char _ebss;
+	char *prev_heap;
 	
 	if (heap == NULL)
 	{
-		heap = (unsigned char *)&__HEAP_START;
+		heap = &_ebss;
 	}
 	prev_heap = heap;
-	/* check removed to show basic approach */
 	
 	heap += incr;
 	return (caddr_t)prev_heap;
