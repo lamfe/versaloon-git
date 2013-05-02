181e853c1d39a8f691da66a049449323dce1e9dc
Simon Qian <simonqian@simonqian.com>
2013-05-02 16:53:47 +0000

    update timing of cfi, longer data set time

--- src/target/cfi/cfi.c
+++ src/target/cfi/cfi.c
@@ -131,11 +131,11 @@ ENTER_PROGRAM_MODE_HANDLER(cfi)
 	cfi_drv_param.nor_info.param.timing.clock_hz_r = 
 		cfi_drv_param.nor_info.param.timing.clock_hz_w = 0;
 	cfi_drv_param.nor_info.param.timing.address_setup_cycle_r = 
-		cfi_drv_param.nor_info.param.timing.address_setup_cycle_w = 2;
+		cfi_drv_param.nor_info.param.timing.address_setup_cycle_w = 0;
 	cfi_drv_param.nor_info.param.timing.address_hold_cycle_r = 
 		cfi_drv_param.nor_info.param.timing.address_hold_cycle_w = 0;
 	cfi_drv_param.nor_info.param.timing.data_setup_cycle_r = 
-		cfi_drv_param.nor_info.param.timing.data_setup_cycle_w = 7;
+		cfi_drv_param.nor_info.param.timing.data_setup_cycle_w = 8;
 	
 	if (mal.init(&cfi_dal_info) || 
 		mal.getinfo(&cfi_dal_info))
